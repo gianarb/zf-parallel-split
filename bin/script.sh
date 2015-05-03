1cbd14dc0076a3e403e3b9c08a3e6bd5f1f84c41
Gianluca Arbezzano <gianarb92@gmail.com>
2015-05-03 23:55:28 +0200

    Update log to s3 and push change into the repo

--- bin/script.sh
+++ bin/script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#apt-get update
-#apt-get install -y python2.7 curl git php5 php5-curl php5-cli
-#curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-#python get-pip.py
-#sudo pip install awscli
+sudo apt-get update
+sudo apt-get install -y python2.7 curl git php5 php5-curl php5-cli
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+sudo pip install awscli
 git clone https://github.com/zendframework/component-split.git
 cd component-split
-./bin/split-component.sh -c %componentName% -t assets/test-files/TestConfiguration.php.dist -i assets/test-files/TestConfiguration.php.travis 2>&1 | tee -a split.log
+./bin/split-component.sh -c %componentName% -t assets/test-files/TestConfiguration.php.dist -i assets/test-files/TestConfiguration.php.travis 2>&1 | tee -a /var/log/split.log
 cd zf2-migrate
+git remote add origin git@github.com:gianarb/zend-%componentName%
+git push origin master
+git push --tags origin
+aws s3 cp /var/log/split.log  s3://zend-split/zend-%componentName%.log
