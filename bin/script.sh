d9a95a50c8d9462ccdd843aa9b9f1bc881004f08
Gianluca Arbezzano <gianarb92@gmail.com>
2015-05-04 15:42:49 +0200

    Refactoring userdata script

--- bin/script.sh
+++ bin/script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y python2.7 curl git php5 php5-curl php5-cli
+apt-get update
+apt-get install -y python2.7 curl git php5 php5-curl php5-cli
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
-sudo pip install awscli
+pip install awscli
+aws s3 cp s3://%sshPath%/id_rsa ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+aws s3 cp s3://%sshPath%/id_rsa.pub ~/.ssh/id_rsa.pub
 git clone https://github.com/zendframework/component-split.git
 cd component-split
-./bin/split-component.sh -c %componentName% -t assets/test-files/TestConfiguration.php.dist -i assets/test-files/TestConfiguration.php.travis 2>&1 | tee -a /var/log/split.log
+./bin/split-component.sh -c %componentName% -t assets/test-files/TestConfiguration.php.dist -i assets/test-files/TestConfiguration.php.travis
 cd zf2-migrate
 git remote add origin git@github.com:gianarb/zend-%componentName%
 git push origin master
 git push --tags origin
-aws s3 cp /var/log/split.log  s3://zend-split/zend-%componentName%.log
-sudo shutdown now
+aws s3 cp /var/log/cloud-init-output.log  s3://%bucketBackup%/zend-%componentName%.log
+shutdown now
--- bin/split.php
+++ bin/split.php
@@ -8,24 +8,40 @@ $config = require __DIR__."/../config/local.php";
 $client = Ec2Client::factory($config['aws']);
 
 foreach($config['components'] as $component) {
+print_r(render($config, $component));die;
     $result = $client->runInstances(array(
         "ImageId" => "ami-d05e75b8",
         "MinCount" => 1,
         "MaxCount" => 1,
-        "InstanceType" => "t2.micro",
+        "InstanceType" => "t2.medium",
         "InstanceInitiatedShutdownBehavior" => "terminate",
-        "KeyName" => "gianarb-def",
-        "UserData" => base64_encode(
-            str_replace(
-                "%componentName%",
-                $component,
-                file_get_contents(__DIR__."/script.sh")
-            )
-        ),
+        "KeyName" => $config['keyName'],
+        "UserData" => base64_encode(render($config, $component)),
         'IamInstanceProfile' => array(
             'Name' => $config['iam']['name'],
         ),
         "SubnetId" => "subnet-5eeb6f75",
     ));
 
+}
+
+function render($config, $component) {
+    $string = str_replace(
+        "%componentName%",
+        $component,
+        file_get_contents(__DIR__."/script.sh")
+    );
+    $string = str_replace(
+        "%bucketBackup%",
+        $config['bucketBackup'],
+        $string
+    );
+
+    $string = str_replace(
+        "%sshPath%",
+        $config['sshPath'],
+        $string
+    );
+
+    return $string;
 }
