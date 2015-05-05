7ebfea0d2ebe029bf546e2c5775def0b11811a9a
Gianluca Arbezzano <gianarb92@gmail.com>
2015-05-05 12:50:13 +0200

    Add github flow to create one repo for component

---
+++ bin/repos.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__."/../vendor/autoload.php";
+
+$config = require __DIR__."/../config/local.php";
+$client = new \Github\Client();
+$client->authenticate($config['githubToken'], null, \Github\Client::AUTH_HTTP_TOKEN);
+foreach($config['components'] as $component) {
+    $name = strtolower($component);
+    try {
+    $repo = $client
+        ->api('repo')
+        ->create("zend-{$name}", "{$component} component from Zend Framework", 'https://github.com/zendframework/zf2', true);
+    } catch (\Github\Exception\ValidationFailedException $e) {
+        echo "[{$e->getCode()}] {$e->getMessage()} \n\r";
+        continue;
+    }
+    echo "[200] Created zend-{$name} \n\r";
+}
