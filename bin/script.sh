451d14b8c7543b645fb9e3687ec8543fbd49e5c0
Gianluca Arbezzano <gianarb92@gmail.com>
2015-05-05 11:59:36 +0200

    Run filter branch in RAM

--- bin/script.sh
+++ bin/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RAM_DISK=3000m
+mount -t tmpfs -o size=$RAM_DISK tmpfs /root
 apt-get update
 apt-get install -y python2.7 curl git php5 php5-curl php5-cli
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
@@ -7,12 +9,14 @@ pip install awscli
 aws s3 cp s3://%sshPath%/id_rsa ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 aws s3 cp s3://%sshPath%/id_rsa.pub ~/.ssh/id_rsa.pub
-git clone https://github.com/zendframework/component-split.git
-cd component-split
-./bin/split-component.sh -c %componentName% -t assets/test-files/TestConfiguration.php.dist -i assets/test-files/TestConfiguration.php.travis
+echo "Host *
+     StrictHostKeyChecking no" > ~/.ssh/config
+git clone https://github.com/zendframework/component-split.git /root/component-split
+cd /root/component-split
+./bin/split-component.sh -c %componentName% -u assets/root-files/phpunit.xml.dist -t assets/root-files/travis.yml
 cd zf2-migrate
 git remote add origin git@github.com:gianarb/zend-%componentName%
 git push origin master
 git push --tags origin
 aws s3 cp /var/log/cloud-init-output.log  s3://%bucketBackup%/zend-%componentName%.log
-shutdown now
+halt
